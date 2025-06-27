323e337163e66615227e13271596e225363d3f85
Mercy Koskey <koskey.mercy@gmail.com>
2025-06-28 02:33:09 +0300

    a script that inserts a single row in the table customer (database alx_book_store

--- task_5.sql
+++ task_5.sql
@@ -1,14 +1,3 @@
 --Write a script that inserts a single row in the table customer (database alx_book_store)
-INSERT INTO CUSTOMERS (
-    CUSTOMER_ID,
-    CUSTOMER_NAME,
-    EMAIL,
-    ADDRESS
-) VALUES (
-    1,
-    'Cole Baidoo',
-    'cbaidoo@sandtech.com',
-    '123 Happiness Ave.'
-);
-
-SELECT 'New customer added successfully!' AS MESSAGE;
+INSERT INTO CUSTOMERS (CUSTOMER_ID, CUSTOMER_NAME, EMAIL, ADDRESS)
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
