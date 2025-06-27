dc02962ac7c060b8f2581b097d61000e53d49d9e
Mercy Koskey <koskey.mercy@gmail.com>
2025-06-28 02:17:56 +0300

    writing a script that prints the full description of the table books from the database

---
+++ task_4.sql
@@ -0,0 +1,15 @@
+--writing a script that prints the full description of the table books from the database
+SELECT 
+    TABLE_NAME,
+    COLUMN_NAME,
+    COLUMN_TYPE,
+    IS_NULLABLE,
+    COLUMN_KEY,
+    COLUMN_DEFAULT,
+    EXTRA
+FROM 
+    INFORMATION_SCHEMA.COLUMNS
+WHERE 
+    TABLE_SCHEMA = DATABASE()
+    AND TABLE_NAME = 'BOOKS';
+
