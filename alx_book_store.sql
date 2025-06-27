19e736fb7aa7b5f46fbaf14fd0358315fa017e93
Mercy Koskey <koskey.mercy@gmail.com>
2025-06-27 19:37:05 +0300

    creating a schema for books database

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -2,6 +2,12 @@
 CREATE DATABASE IF NOT EXISTS alx_book_store;
 USE alx_book_store;
 
+--CREATE AUTHORS TABLE
+CREATE TABLE IF NOT EXISTS Authors(
+    author_id INT AUTO_INCREMENT PRIMARY KEY,
+    author_name VARCHAR(215) NOT NULL
+);
+
 -- Create Books table
 CREATE TABLE IF NOT EXISTS Books (
     book_id INT AUTO_INCREMENT PRIMARY KEY,
@@ -12,18 +18,12 @@ CREATE TABLE IF NOT EXISTS Books (
     FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
 
---CREATE AUTHORS TABLE
-CREATE TABLE IF NOT EXISTS Authors(
-    author_id INT AUTO_INCREMENT PRIMARY KEY,
-    author_name VARCHAR(215) NOT NULL,
-);
-
 --CREATE CUSTOMERS TABLE
 CREATE TABLE IF NOT EXISTS Customers(
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_name VARCHAR(215) NOT NULL,
     email VARCHAR(215) NOT NULL,
-    address TEXT NOT NULL,
+    address TEXT NOT NULL
 );
 
 --CREATE ORDERS TABLE
@@ -31,7 +31,7 @@ CREATE TABLE IF NOT EXISTS Orders(
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT NOT NULL,
     order_date DATE NOT NULL,
-    FOREIGN KEY (customer_id) REFERENCES Customers (customer_id),
+    FOREIGN KEY (customer_id) REFERENCES Customers (customer_id)
 );
 
 --CREATE ORDERS_DETAILS TABLE
@@ -41,7 +41,7 @@ CREATE TABLE IF NOT EXISTS Orders_Details(
     book_id INT NOT NULL,
     quantity DOUBLE NOT NULL,
     FOREIGN KEY (order_id) REFERENCES Orders (order_id),
-    FOREIGN KEY (book_id) REFERENCES Books (book_id),
+    FOREIGN KEY (book_id) REFERENCES Books (book_id)
 );
 
 
