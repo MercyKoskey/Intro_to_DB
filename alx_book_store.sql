30cd5ec68f9992e9d75310faf0feeb9eed0abd94
Mercy Koskey <koskey.mercy@gmail.com>
2025-06-27 18:53:34 +0300

    creating a schema for books database

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -4,40 +4,40 @@ USE alx_book_store
 
 --CREATE BOOKS TABLE
 CREATE TABLE IF NOT EXISTS Books(
-    book_id (Primary Key)
-    title VARCHAR(130)
-    author_id (Foreign Key referencing Authors table)
-    price DOUBLE
-    publication_date DATE 
+    book_id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(130),
+    author_id (Foreign Key referencing Authors table),
+    price DOUBLE,
+    publication_date DATE,
 );
 
 --CREATE AUTHORS TABLE
 CREATE TABLE IF NOT EXISTS Authors(
-    author_id (Primary Key)
-    author_name VARCHAR(215)
+    author_id INT AUTO_INCREMENT PRIMARY KEY,
+    author_name VARCHAR(215),
 );
 
 --CREATE CUSTOMERS TABLE
 CREATE TABLE IF NOT EXISTS Customers(
-    customer_id (Primary Key)
-    customer_name VARCHAR(215)
-    email VARCHAR(215)
-    address TEXT
+    customer_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_name VARCHAR(215),
+    email VARCHAR(215),
+    address TEXT,
 );
 
 --CREATE ORDERS TABLE
 CREATE TABLE IF NOT EXISTS Orders(
-order_id (Primary Key)
-customer_id (Foreign Key referencing Customers table)
-order_date DATE
+order_id INT AUTO_INCREMENT PRIMARY KEY,
+customer_id (Foreign Key referencing Customers table),
+order_date DATE,
 );
 
 --CREATE ORDERS_DETAILS TABLE
 CREATE TABLE IF NOT EXISTS Orders_Details(
-    orderdetailid (Primary Key)
-    order_id (Foreign Key referencing Orders table)
-    book_id (Foreign Key referencing Books table)
-    quantity DOUBLE
+    orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
+    order_id (Foreign Key referencing Orders table),
+    book_id (Foreign Key referencing Books table),
+    quantity DOUBLE,
 );
 
 
